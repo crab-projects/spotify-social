8145c09904047ebb313788c809ff37609631f54c
Rustom Ichhaporia <45826892+rustom@users.noreply.github.com>
2021-03-05 23:50:17 -0600

    ⚛️ Create React router and basic home page (#15)
    * Begin converting to typescript
    
    * Begin converting to typescript
    
    * Set up react router and react folders
    
    Co-authored-by: Rustom Ichhaporia <45826892+rustom-ichhaporia@users.noreply.github.com>

---
+++ client/src/pages/playlist-page.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import { Link } from 'react-router-dom';
+import { Layout } from '../components';
+
+export default function PlaylistPage() {
+  return (
+    <Layout>
+      <p>The Playlist page.</p>
+      <Link to="/">Sweet Home Alabama.</Link>
+    </Layout>
+  );
+}
