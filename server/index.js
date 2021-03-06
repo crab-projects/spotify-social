e9186325d607d6cd35803a68933e2c680be26d00
Alec Chen <alecchendev@gmail.com>
2021-03-05 21:52:43 -0600

    Setup basic commands for initial deployment

--- server/index.js
+++ server/index.js
@@ -7,8 +7,11 @@ const app = express();
 //app.use(cors());
 
 // Serve static files from the React app
-app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(express.static(path.join(__dirname, '../client/build')));
 
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 const port = process.env.PORT || 5000 || '0.0.0.0';
 app.listen(port);
