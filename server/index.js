c3d84856a4f0f0a844908ff2c85cd89b1fe10b51
Alec Chen <alecchendev@gmail.com>
2021-03-05 23:31:47 -0600

    Begin implementing https request for spotify api flow

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@
         if (v !== undefined) module.exports = v;
     }
     else if (typeof define === "function" && define.amd) {
-        define(["require", "exports", "express"], factory);
+        define(["require", "exports", "express", "https"], factory);
     }
 })(function (require, exports) {
     "use strict";
@@ -23,6 +23,30 @@
     });
     var port = process.env.PORT || 5000 || '0.0.0.0';
     app.listen(port);
+    /* Load the HTTP library */
+    var https = require("https");
+    /* Create an HTTP server to handle responses */
+    // http.createServer(function(request, response) {
+    //   response.writeHead(200, {"Content-Type": "text/plain"});
+    //   response.write("Hello World");
+    //   response.end();
+    // }).listen(8888);
+    var options = {
+        hostname: 'whatever.com',
+        port: 443,
+        path: '/todos',
+        method: 'GET'
+    };
+    var req = https.request(options, function (res) {
+        console.log("statusCode: " + res.statusCode);
+        res.on('data', function (d) {
+            process.stdout.write(d);
+        });
+    });
+    req.on('error', function (error) {
+        console.error(error);
+    });
+    req.end();
     console.log("spotify-social listening on " + port);
 });
 //# sourceMappingURL=index.js.map
--- server/index.ts
+++ server/index.ts
@@ -16,4 +16,36 @@ app.get('*', (req, res) => {
 const port = process.env.PORT || 5000 || '0.0.0.0';
 app.listen(port);
 
+/* Load the HTTP library */
+import https = require("https");
+
+/* Create an HTTP server to handle responses */
+
+// http.createServer(function(request, response) {
+//   response.writeHead(200, {"Content-Type": "text/plain"});
+//   response.write("Hello World");
+//   response.end();
+// }).listen(8888);
+
+const options = {
+  hostname: 'whatever.com',
+  port: 443,
+  path: '/todos',
+  method: 'GET'
+}
+
+const req = https.request(options, res => {
+  console.log(`statusCode: ${res.statusCode}`)
+
+  res.on('data', d => {
+    process.stdout.write(d)
+  })
+})
+
+req.on('error', error => {
+  console.error(error)
+})
+
+req.end()
+
 console.log(`spotify-social listening on ${port}`);
