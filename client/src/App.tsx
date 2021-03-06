e8b8ac5baf7fed805ad3fdaac245bf100b028340
Rustom Ichhaporia <45826892+rustom-ichhaporia@users.noreply.github.com>
2021-03-05 23:04:31 -0600

    Begin converting to typescript

---
+++ client/src/App.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+
+import { Home, PlaylistPage } from './pages/index';
+
+function App() {
+  return (
+    <main>
+      <Switch>
+        <Route path="/" component={Home} exact />
+        <Route path="/make" component={MakeMeeting} />
+        <Route path="/meetingLogin/:meetingID" component={MeetingLogin} />
+        <Route
+          path="/meeting/:meetingID/password/:password"
+          component={Meeting}
+        />
+      </Switch>
+    </main>
+  );
+}
+
+export default App;
---
+++ client/src/pages/playlist-page.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import Layout from '.'
+
+export default function PlaylistPage () {
+
+}
